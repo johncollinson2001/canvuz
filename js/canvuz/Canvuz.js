2dfea948204a0663af44196924d024943adb00db
John Collinson <mail@johncollinson.info>
2017-02-03 23:47:14 +0000

    changed captions and added comments

--- js/canvuz/Canvuz.js
+++ js/canvuz/Canvuz.js
@@ -30,79 +30,122 @@ Canvuz.load = function(_elem, _domain) {
 	Canvuz.canvas.addAlbum("Urban Landscapes", "1");				
 	
 	//photo loader
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/001.jpg", "EITP crowd going nuts!", "1");
-	Canvuz.canvas.albums["1"].photos["1"].addComment("This photo is amazing, it makes me wet. What a nice castle. hsdfuidshfiudsf dsfhudsif dsuf hiuds faui", "John Collinson", "Wednesday 25th of August 2010 at 21:20");	
-	Canvuz.canvas.albums["1"].photos["1"].addComment("This photo is great, its just bloody great.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
-	Canvuz.canvas.albums["1"].photos["1"].addComment("Yes oh yes, i like this. I really do.", "Helen Collinson", "Wednesday 25th of August 2010 at 13:40");	
-	Canvuz.canvas.albums["1"].photos["1"].addComment("Christ, this photo is fucking great.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/002.jpg", "Bionic tent going off!", "2", true);
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/003.jpg", "Party time...", "3");
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/004.jpg", "MC Shocker working the crowd... Grr!", "4");
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/001.jpg", "Great view of the city.", "1");
+	Canvuz.canvas.albums["1"].photos["1"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["1"].photos["1"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/002.jpg", "A coast like no other.", "2", true);
+	Canvuz.canvas.albums["1"].photos["2"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/003.jpg", "What?", "3");
+	Canvuz.canvas.albums["1"].photos["3"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/004.jpg", "Wow what a view!", "4");
 	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/005.jpg", "George and his smug grin.", "5");
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/006.jpg", "Jules rocking the hairstyle.", "6");				
-	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/007.jpg", "Warming up.", "7");				
+	Canvuz.canvas.albums["1"].photos["5"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["1"].photos["5"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/006.jpg", "One of the nicest places on earth.", "6");				
+	Canvuz.canvas.albums["1"].addPhoto(300, 325, "demo/city/007.jpg", "Bananas are better than apples.", "7");				
+	Canvuz.canvas.albums["1"].photos["7"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
 			
 	//album loader
 	Canvuz.canvas.addAlbum("Coastal Landscapes", "2");				
 	
 	//photo loader
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/001.jpg", "EITP crowd going nuts!", "1");
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/002.jpg", "Bionic tent going off!", "2");
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/003.jpg", "Party time...", "3");
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/004.jpg", "MC Shocker working the crowd... Grr!", "4");
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/001.jpg", "A memorable trip.", "1");
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/002.jpg", "A coast like no other.", "2");
+	Canvuz.canvas.albums["2"].photos["2"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["2"].photos["2"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");		
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/003.jpg", "Err, say what?!", "3");
+	Canvuz.canvas.albums["2"].photos["3"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/004.jpg", "You know what i mean!", "4");
+	Canvuz.canvas.albums["2"].photos["4"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
 	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/005.jpg", "George and his smug grin.", "5", true);
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/006.jpg", "Jules rocking the hairstyle.", "6");				
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/007.jpg", "Warming up.", "7");
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/008.jpg", "Jules rocking the hairstyle.", "8");				
-	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/009.jpg", "Warming up.", "9");
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/006.jpg", "West and far right East.", "6");		
+	Canvuz.canvas.albums["2"].photos["6"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["2"].photos["6"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");		
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/007.jpg", "Bananas are better than apples.", "7");
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/008.jpg", "West and far right East.", "8");		
+	Canvuz.canvas.albums["2"].photos["8"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["2"].photos["8"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["2"].photos["8"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["2"].addPhoto(300, 325, "demo/coast/009.jpg", "Bananas are better than apples.", "9");
 	
 	//album loader
 	Canvuz.canvas.addAlbum("Forest Landscapes", "3");				
 	
 	//photo loader
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/001.jpg", "EITP crowd going nuts!", "1");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/002.jpg", "Bionic tent going off!", "2");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/003.jpg", "Party time...", "3");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/004.jpg", "MC Shocker working the crowd... Grr!", "4");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/001.jpg", "A memorable trip.", "1");
+	Canvuz.canvas.albums["3"].photos["1"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/002.jpg", "A coast like no other.", "2");
+	Canvuz.canvas.albums["3"].photos["2"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["3"].photos["2"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["3"].photos["2"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/003.jpg", "Err, say what?!", "3");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/004.jpg", "You know what i mean!", "4");
+	Canvuz.canvas.albums["3"].photos["4"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
 	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/005.jpg", "George and his smug grin.", "5");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/006.jpg", "Jules rocking the hairstyle.", "6", true);				
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/007.jpg", "Warming up.", "7");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/008.jpg", "Jules rocking the hairstyle.", "8");				
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/009.jpg", "Warming up.", "9");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/010.jpg", "Warming up.", "10");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/011.jpg", "Warming up.", "11");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/012.jpg", "Warming up.", "12");
-	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/013.jpg", "Warming up.", "13");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/006.jpg", "West and far right East.", "6", true);		
+	Canvuz.canvas.albums["3"].photos["6"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["3"].photos["6"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["3"].photos["6"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/007.jpg", "Bananas are better than apples.", "7");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/008.jpg", "West and far right East.", "8");				
+	Canvuz.canvas.albums["3"].photos["8"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/009.jpg", "Bananas are better than apples.", "9");
+	Canvuz.canvas.albums["3"].photos["9"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["3"].photos["9"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["3"].photos["9"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/010.jpg", "Well, you know! You know? You know.", "10");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/011.jpg", "Bananas are better than apples.", "11");
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/012.jpg", "11 out of 10!!! 	", "12");
+	Canvuz.canvas.albums["3"].photos["12"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["3"].addPhoto(300, 325, "demo/forest/013.jpg", "Bananas are better than apples.", "13");
 	
 	//album loader
 	Canvuz.canvas.addAlbum("Lake Landscapes", "4");				
 	
 	//photo loader
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/001.jpg", "EITP crowd going nuts!", "1");
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/002.jpg", "Bionic tent going off!", "2", true);
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/003.jpg", "Party time...", "3");
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/004.jpg", "MC Shocker working the crowd... Grr!", "4");
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/001.jpg", "A memorable trip.", "1");	
+	Canvuz.canvas.albums["4"].photos["1"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["4"].photos["1"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/002.jpg", "A coast like no other.", "2", true);
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/003.jpg", "Err, say what?!", "3");
+	Canvuz.canvas.albums["4"].photos["3"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/004.jpg", "You know what i mean!", "4");
 	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/005.jpg", "George and his smug grin.", "5");
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/006.jpg", "Jules rocking the hairstyle.", "6");				
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/007.jpg", "Warming up.", "7");
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/008.jpg", "Jules rocking the hairstyle.", "8");				
-	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/009.jpg", "Warming up.", "9");
+	Canvuz.canvas.albums["4"].photos["5"].addComment("This photo is great, wish i was there.", "Peter Taylor", "Wednesday 25th of August 2010 at 21:40");	
+	Canvuz.canvas.albums["4"].photos["5"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/006.jpg", "West and far right East.", "6");				
+	Canvuz.canvas.albums["4"].photos["6"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/007.jpg", "Cashback.", "7");
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/008.jpg", "West and far right East.", "8");				
+	Canvuz.canvas.albums["4"].addPhoto(300, 325, "demo/lakes/009.jpg", "Bananas are better than apples.", "9");
+	Canvuz.canvas.albums["4"].photos["9"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");	
 	
 	//album loader
 	Canvuz.canvas.addAlbum("Mountain Landscapes", "5");				
 	
 	//photo loader
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/001.jpg", "EITP crowd going nuts!", "1");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/002.jpg", "Bionic tent going off!", "2");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/003.jpg", "Party time...", "3");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/004.jpg", "MC Shocker working the crowd... Grr!", "4");
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/001.jpg", "A memorable trip.", "1");
+	Canvuz.canvas.albums["5"].photos["1"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].photos["1"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/002.jpg", "A coast like no other.", "2");
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/003.jpg", "Err, say what?!", "3");
+	Canvuz.canvas.albums["5"].photos["3"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/004.jpg", "You know what i mean!", "4");
+	Canvuz.canvas.albums["5"].photos["4"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].photos["4"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
 	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/005.jpg", "George and his smug grin.", "5");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/006.jpg", "Jules rocking the hairstyle.", "6");				
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/007.jpg", "Warming up.", "7");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/008.jpg", "Jules rocking the hairstyle.", "8", true);				
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/009.jpg", "Warming up.", "9");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/010.jpg", "Warming up.", "10");
-	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/011.jpg", "Warming up.", "11");						
+	Canvuz.canvas.albums["5"].photos["5"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/006.jpg", "West and far right East.", "6");				
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/007.jpg", "Bananas are better than apples.", "7");
+	Canvuz.canvas.albums["5"].photos["7"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].photos["7"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/008.jpg", "West and far right East.", "8", true);				
+	Canvuz.canvas.albums["5"].photos["8"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/009.jpg", "Bananas are better than apples.", "9");
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/010.jpg", "What you on about?", "10");
+	Canvuz.canvas.albums["5"].photos["10"].addComment("Yes oh yes, i like this. I really do.", "Helen Jenkins", "Wednesday 25th of August 2010 at 13:40");	
+	Canvuz.canvas.albums["5"].photos["10"].addComment("What memories.", "George Mclelland", "Wednesday 25th of August 2010 at 16:33");			
+	Canvuz.canvas.albums["5"].addPhoto(300, 325, "demo/mountain/011.jpg", "Bananas are better than apples.", "11");						
 	
 	//add menu	
 	Canvuz.canvas.addMenu();	
